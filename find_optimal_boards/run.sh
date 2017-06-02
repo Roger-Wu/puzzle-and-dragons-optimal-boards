faaf02a8539ad9f484193e3878f66a556207cecd
Roger-Wu <gsetcrw@gmail.com>
2017-06-03 03:09:48 +0800

    commands

---
+++ find_optimal_boards/run.sh
@@ -0,0 +1,68 @@
+THREADS=4
+
+pypy3 find_optimal_boards.py 26 4 3 -t $THREADS
+pypy3 find_optimal_boards.py 26 3 1 3 -t $THREADS
+pypy3 find_optimal_boards.py 26 2 2 2 -t $THREADS
+
+pypy3 find_optimal_boards.py 25 5 4 -t $THREADS
+pypy3 find_optimal_boards.py 25 4 1 4 -t $THREADS
+pypy3 find_optimal_boards.py 25 3 2 4 -t $THREADS
+
+pypy3 find_optimal_boards.py 24 6 5 -t $THREADS
+pypy3 find_optimal_boards.py 24 5 1 4 -t $THREADS
+pypy3 find_optimal_boards.py 24 4 2 4 -t $THREADS
+pypy3 find_optimal_boards.py 24 3 3 5 -t $THREADS
+
+pypy3 find_optimal_boards.py 23 7 6 -t $THREADS
+pypy3 find_optimal_boards.py 23 6 1 6 -t $THREADS
+pypy3 find_optimal_boards.py 23 5 2 5 -t $THREADS
+pypy3 find_optimal_boards.py 23 4 3 6 -t $THREADS
+pypy3 find_optimal_boards.py 23 3 3 1 6 -t $THREADS
+
+pypy3 find_optimal_boards.py 22 8 7 -t $THREADS
+pypy3 find_optimal_boards.py 22 7 1 7 -t $THREADS
+pypy3 find_optimal_boards.py 22 6 2 7 -t $THREADS
+pypy3 find_optimal_boards.py 22 5 3 7 -t $THREADS
+pypy3 find_optimal_boards.py 22 4 4 7 -t $THREADS
+pypy3 find_optimal_boards.py 22 3 3 2 7 -t $THREADS
+
+pypy3 find_optimal_boards.py 21 9 8 -t $THREADS
+pypy3 find_optimal_boards.py 21 8 1 7 -t $THREADS
+pypy3 find_optimal_boards.py 21 7 2 7 -t $THREADS
+pypy3 find_optimal_boards.py 21 6 3 8 -t $THREADS
+pypy3 find_optimal_boards.py 21 5 4 7 -t $THREADS
+pypy3 find_optimal_boards.py 21 3 3 3 7 -t $THREADS
+
+pypy3 find_optimal_boards.py 20 10 8 -t $THREADS
+pypy3 find_optimal_boards.py 20 9 1 8 -t $THREADS
+pypy3 find_optimal_boards.py 20 8 2 8 -t $THREADS
+pypy3 find_optimal_boards.py 20 7 3 9 -t $THREADS
+pypy3 find_optimal_boards.py 20 6 4 9 -t $THREADS
+pypy3 find_optimal_boards.py 20 5 5 8 -t $THREADS
+pypy3 find_optimal_boards.py 20 5 3 2 8 -t $THREADS
+pypy3 find_optimal_boards.py 20 3 3 3 1 9 -t $THREADS
+
+pypy3 find_optimal_boards.py 19 11 9 -t $THREADS
+pypy3 find_optimal_boards.py 19 10 1 9 -t $THREADS
+pypy3 find_optimal_boards.py 19 9 2 9 -t $THREADS
+pypy3 find_optimal_boards.py 19 8 3 9 -t $THREADS
+pypy3 find_optimal_boards.py 19 7 4 9 -t $THREADS
+pypy3 find_optimal_boards.py 19 6 5 9 -t $THREADS
+pypy3 find_optimal_boards.py 19 3 3 3 2 9 -t $THREADS
+
+pypy3 find_optimal_boards.py 18 12 9 -t $THREADS
+pypy3 find_optimal_boards.py 18 11 1 9 -t $THREADS
+pypy3 find_optimal_boards.py 18 10 2 9 -t $THREADS
+pypy3 find_optimal_boards.py 18 9 3 9 -t $THREADS
+pypy3 find_optimal_boards.py 18 8 4 9 -t $THREADS
+pypy3 find_optimal_boards.py 18 7 5 9 -t $THREADS
+pypy3 find_optimal_boards.py 18 6 6 9 -t $THREADS
+pypy3 find_optimal_boards.py 18 5 5 2 8 -t $THREADS
+pypy3 find_optimal_boards.py 18 3 3 3 3 9 -t $THREADS
+
+pypy3 find_optimal_boards.py 17 13 9 -t $THREADS
+pypy3 find_optimal_boards.py 17 3 3 3 3 1 9 -t $THREADS
+
+pypy3 find_optimal_boards.py 16 14 9 -t $THREADS
+pypy3 find_optimal_boards.py 16 3 3 3 3 2 9 -t $THREADS
+
