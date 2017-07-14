b34086420e35529910bcba6c4dd89f64711db2b4
Roger-Wu <gsetcrw@gmail.com>
2017-07-15 05:57:20 +0800

    attach animation link to boards

--- visualize_result/js/BoardCardsContainer.jsx
+++ visualize_result/js/BoardCardsContainer.jsx
@@ -4,48 +4,79 @@ import { render } from "react-dom";
 
 class Board extends React.Component {
   render() {
-    var rows = this.props.board.map(function(row_str, index) {
-      var color_strs = row_str.split(" ");
-      var row_orbs = color_strs.map(function(color_str, index) {
+    let { board } = this.props;
+
+    let int_board = [];
+    let board_str = "";
+    for (let row_str of board) {
+      let row = [];
+      for (let color_str of row_str.split(" ")) {
+        let color_idx = parseInt(color_str);
+        row.push(color_idx);
+        board_str += (color_idx - 1).toString();
+      }
+      int_board.push(row)
+    }
+    let url = "http://serizawa.web5.jp/puzzdra_theory_maker/index.html?layout=" + board_str + "&route=05,";
+    let rows = this.props.board.map(function(row_str, index) {
+      let color_strs = row_str.split(" ");
+      let row_orbs = color_strs.map(function(color_str, index) {
         return React.createElement("span", {className: "orb orb-color-" + color_str, key: index}, null);
       });
       return React.createElement("div", {className: "board-row", key: index}, row_orbs);
     });
-    return React.createElement("div", {className: "board"}, rows);
+    return (
+      <a href={url} target="_blank">
+        {React.createElement("div", {className: "board"}, rows)}
+      </a>
+    )
   }
 }
 
 export class BoardCard extends React.Component {
   render() {
-    var { board_obj, title } = this.props;
-    // var infos = [
+    let { board_obj, title } = this.props;
+    // let infos = [
     //   `${board_obj.combo_count} combos`,
     //   `${board_obj.main_combo_count} main combos`,
     //   `${board_obj.matched_main_count} matched main orbs`,
     //   `${board_obj.drop_times} times of dropping`,
     // ];
+
     return React.createElement("div", {className: "board-card"}, [
       React.createElement("div", {className: "board-index", key: "board-index"}, `${title}`),
       <div className="board-info" key="board-info">
         <div>
+          <span className="board-info-text">combos</span>
           <span className="board-info-number emphasis">
             <span>{board_obj.main_combo_count}</span>
             <span className="board-info-plus">+</span>
             <span>{board_obj.combo_count - board_obj.main_combo_count}</span>
           </span>
-          <span className="board-info-text"> combos</span>
         </div>
         <div>
+          <span className="board-info-text">orbs matched</span>
           <span className="board-info-number emphasis">
             <span>{board_obj.matched_main_count}</span>
             <span className="board-info-plus">+</span>
             <span>{board_obj.matched_other_count}</span>
           </span>
-          <span className="board-info-text"> orbs matched</span>
         </div>
         <div>
+          <span className="board-info-text">avg. combos</span>
+          <span className="board-info-number emphasis">{board_obj.combo_count_avg.toFixed(1)}</span>
+        </div>
+        <div>
+          <span className="board-info-text">avg. damage</span>
+          <span className="board-info-number emphasis">x {board_obj.main_damage_multiplier_avg.toFixed(1)}</span>
+        </div>
+        <div>
+          <span className="board-info-text">times of dropping</span>
           <span className="board-info-number emphasis">{board_obj.drop_times}</span>
-          <span className="board-info-text"> times of dropping</span>
+        </div>
+        <div>
+          <span className="board-info-text">min. combos</span>
+          <span className="board-info-number emphasis">{board_obj.combo_count_min}</span>
         </div>
       </div>,
       // infos.map(function(info) {
